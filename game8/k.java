afa0c494d62bc788c15b8aeba02d7b16466e349c
kousei28 <20109@hakodate.kosen-ac.jp>
2024-10-21 14:17:49 +0900

    kがキャラ追加

---
+++ game8/k.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class k here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class k extends Actor
+{
+    /**
+     * Act - do whatever the k wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
