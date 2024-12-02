9902670d029744c6f460e3aea1d5333f9b4f21fb
TakinoShuto <20105@hakodate.kosen-ac.jp>
2024-12-02 13:49:58 +0900

    Aga

--- game8/MyWorld.java
+++ game8/MyWorld.java
@@ -1,21 +1,18 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;  // Greenfootの基本クラスをインポート
 
-/**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class MyWorld extends World
 {
-
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
-    public MyWorld()
+    public MyWorld() 
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        // 画面サイズを設定 (横幅600px、縦幅400px)
         super(600, 400, 1); 
+        
+        // 背景画像を設定
+        GreenfootImage backgroundImage = new GreenfootImage("background.png");  // 背景画像を読み込む
+        backgroundImage.scale(getWidth(), getHeight());  // 画面サイズにリサイズ
+        setBackground(backgroundImage);  // リサイズされた画像を背景として設定
+
+        A a = new A();// 生徒アクターを世界に追加
+        addObject(a,300,100);
     }
 }
